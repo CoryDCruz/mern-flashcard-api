39cb1848b9697fd22e4d2f077c9d00abe1566875
Cory Dela Cruz <corydcruz@gmail.com>
2022-08-24 15:47:17 -0700

    initial server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const cors = require("cors")
+const morgan = require('morgan')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const { PORT, DATABASE_URL } = process.env
+const app = express()
+
+mongoose.connect(DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+
+mongoose.connection
+  .on("open", () => {console.log("Connected to Mongoose")})
+  .on("close", () => {console.log("Disconnected to Mongoose")})
+  .on("error", (error) => {console.log(error)})
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.send("Welcome to the Flashcard API")
+})
+
+app.listen(PORT, () => {
+  console.log("Running on: " + PORT)
+})
