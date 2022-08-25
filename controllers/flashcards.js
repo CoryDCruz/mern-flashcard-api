a0af8bd31952fc5807399c127fff66cb76c50b7e
Cory Dela Cruz <corydcruz@gmail.com>
2022-08-24 22:24:34 -0700

    delete and show routes for flashcard

--- controllers/flashcards.js
+++ controllers/flashcards.js
@@ -3,16 +3,49 @@ const Decks = require('../models/deck')
 const flashcardsRouter = require('express').Router()
 
 //Delete
+flashcardsRouter.delete('/:id', async (req,res) => {
+  //TODO: need to also add in logic that removes the card id from the deck array
+  try {
+    res.status(200).json(
+      await Flashcard.findByIdAndRemove(req.params.id)
+      .then((deletedCard) => {
+        return Decks.findByIdAndUpdate(
+          deletedCard.deck,
+          { $pull: { flashcards: deletedCard._id } },
+          { new: true }
+          )
+      })
+      )
+  } catch (error) {
+    res.status(400).json({ message: "Bad request" })
+  }
+})
 
 
 //Update
+flashcardsRouter.put('/:id', async (req, res) => {
+  try {
+    res.status(200).json(await Flashcard.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    ))
+  } catch (error) {
+    
+  }
+})
 
 //Create
 flashcardsRouter.post('/:id', async (req,res) => {
   console.log(req.params.id)
     try {
       res.status(200).json(
-        await Flashcard.create(req.body)
+        await Flashcard.create(
+          { front: req.body.front , 
+            back: req.body.back , 
+            deck: req.params.id
+          }
+          )
         .then((flashcard) => {
           return Decks.findByIdAndUpdate(
             req.params.id,
@@ -28,5 +61,13 @@ flashcardsRouter.post('/:id', async (req,res) => {
 
 
 //Show
+
+flashcardsRouter.get('/:id', async (req, res) => {
+  try {
+    res.status(200).json(await Flashcard.findById(req.params.id))
+  } catch (error) {
+    res.status(400).json( { message: "Bad request" })
+  }
+})
 
 module.exports = flashcardsRouter
